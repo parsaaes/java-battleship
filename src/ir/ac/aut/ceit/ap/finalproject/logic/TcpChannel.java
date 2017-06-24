64a7eca058f31ef04b5930f031a0c36c6c3dc0dc
Amirali <Amirali.Sajjadi98@gmail.com>
2017-06-24 19:20:00 +0430

    from Amirali-TCP added

---
+++ src/ir/ac/aut/ceit/ap/finalproject/logic/TcpChannel.java
@@ -0,0 +1,24 @@
+package ir.ac.aut.ceit.ap.finalproject.logic;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class TcpChannel {
+    private Socket mSocket;
+    private OutputStream mOutputStream;
+    private InputStream mInputStream;
+//
+//    public TcpChannel(SocketAddress socketAddress, int timeout)
+//    public TcpChannel(Socket socket, int timeout)
+//    /** * Try to read specific count from input stream. */
+//    public byte[] read(final int count)
+//    /** * Write bytes on output stream. */
+//    public void write(byte[] data)
+//    /** * Check socket’s connectivity. */
+//    public boolean isConnected()
+//    /** * Try to close socket and input-output streams. */
+//    public void closeChannel()
+}
