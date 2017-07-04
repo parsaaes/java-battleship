3b00ffca340f6f30c76182b0ad2cb0fb8d65671a
Parsa <s.liceyourhead@gmail.com>
2017-07-04 13:27:59 +0430

    flush for write + deployed closing method + some little improvement for read method

--- src/ir/ac/aut/ceit/ap/finalproject/logic/TcpChannel.java
+++ src/ir/ac/aut/ceit/ap/finalproject/logic/TcpChannel.java
@@ -1,11 +1,9 @@
 package ir.ac.aut.ceit.ap.finalproject.logic;
 
-import sun.rmi.transport.tcp.TCPChannel;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.net.SocketException;
@@ -44,36 +42,40 @@ public class TcpChannel {
 
     public byte[] read(final int count) {
         byte[] bytes = new byte[count];
+        int c;
 
         try {
             mInputStream = mSocket.getInputStream();
             for (int i = 0; i < bytes.length; i++) {
-                int c;
-                if((c=mInputStream.read())!= -1 ) {
-                    bytes[i] = (byte)c;
+                if ((c = mInputStream.read()) != -1) {
+                    bytes[i] = (byte) c;
                 }
-
-                    }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return bytes;
+
     }
 
     public void write(byte[] data) {
         try {
             mOutputStream = mSocket.getOutputStream();
             mOutputStream.write(data);
+            mOutputStream.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
-//    public byte[] read(final int count)
-//    /** * Write bytes on output stream. */
-//    public void write(byte[] data)
-//    /** * Check socket’s connectivity. */
-//    public boolean isConnected()
-//    /** * Try to close socket and input-output streams. */
-//    public void closeChannel()
+
+    public void closeChannel(){
+        try {
+            mInputStream.close();
+            mOutputStream.close();
+            mSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
