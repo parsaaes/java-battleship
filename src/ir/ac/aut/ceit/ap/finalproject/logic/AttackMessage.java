dba7d7b50db00684d134afd86d8d45af5792a5c8
Amirali <Amirali.Sajjadi98@gmail.com>
2017-07-09 16:57:19 +0430

    Added AttackMessage

---
+++ src/ir/ac/aut/ceit/ap/finalproject/logic/AttackMessage.java
@@ -0,0 +1,18 @@
+package ir.ac.aut.ceit.ap.finalproject.logic;
+
+public class AttackMessage extends BaseMessage {
+    @Override
+    protected void serialize() {
+
+    }
+
+    @Override
+    protected void deserialize() {
+
+    }
+
+    @Override
+    public byte getMessageType() {
+        return 0;
+    }
+}
