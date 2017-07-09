52c5d495439f4439ee19f93f1e269a93d5a375cb
Amirali <Amirali.Sajjadi98@gmail.com>
2017-07-09 17:43:05 +0430

    Added TurnMessage

---
+++ src/ir/ac/aut/ceit/ap/finalproject/logic/TurnMessage.java
@@ -0,0 +1,21 @@
+package ir.ac.aut.ceit.ap.finalproject.logic;
+
+/**
+ * Created by NP on 7/9/2017.
+ */
+public class TurnMessage extends BaseMessage {
+    @Override
+    protected void serialize() {
+
+    }
+
+    @Override
+    protected void deserialize() {
+
+    }
+
+    @Override
+    public byte getMessageType() {
+        return 0;
+    }
+}
