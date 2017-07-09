a54267a0b1556df746a58a8082b8600f3398a851
Parsa <s.liceyourhead@gmail.com>
2017-07-09 23:15:51 +0430

    added music ( music file did not added ) + one simple json file

--- src/ir/ac/aut/ceit/ap/finalproject/Main.java
+++ src/ir/ac/aut/ceit/ap/finalproject/Main.java
@@ -5,10 +5,29 @@ import ir.ac.aut.ceit.ap.finalproject.view.GuestWaitingFrame;
 import ir.ac.aut.ceit.ap.finalproject.view.LoginFrame;
 import ir.ac.aut.ceit.ap.finalproject.view.MainFrame;
 import ir.ac.aut.ceit.ap.finalproject.view.SplashScreen;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Main {
 
     public static void main(String[] args) {
+        Thread musicThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Player player = new Player(new FileInputStream("game.mp3"));
+                    player.play();
+                } catch (JavaLayerException e) {
+                    System.out.println("no music");
+                } catch (FileNotFoundException e) {
+                    System.out.println("no music");
+                }
+            }
+        });
+        musicThread.start();
         SplashScreen splashScreen = new SplashScreen("pic.png");
         splashScreen.show(5000);
         try {
