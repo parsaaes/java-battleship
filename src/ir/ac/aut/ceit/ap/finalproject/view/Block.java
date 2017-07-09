7523d3271d401b898156a09bbbbc46e21f082d1c
Amirali <Amirali.Sajjadi98@gmail.com>
2017-07-09 10:30:48 +0430

    deployed Block constructor

--- src/ir/ac/aut/ceit/ap/finalproject/view/Block.java
+++ src/ir/ac/aut/ceit/ap/finalproject/view/Block.java
@@ -1,7 +1,38 @@
 package ir.ac.aut.ceit.ap.finalproject.view;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by NP on 7/9/2017.
  */
-public class Block {
+public class Block extends JButton {
+    private int blockStatus;
+    private int xCord;
+    private int yCord;
+
+    /*
+    blockStatus =>
+    ------- sunk your board :
+    0 -> free [cyan]
+    1 -> a ship is here [black]
+    2 -> cant be sunk (around a ship) [white]
+
+    ------- enemy's board :
+    3 -> not approved attack
+    4 -> approved attack
+     */
+
+    public Block(String text, int xCord, int yCord, int blockStatus) {
+        super(text);
+        super.setBackground(Color.CYAN);
+        this.xCord = xCord;
+        this.yCord = yCord;
+        this.blockStatus = blockStatus;
+        super.setPreferredSize(new Dimension(32, 32));
+        super.setSize(new Dimension(32, 32));
+    }
+
+
 }
+
